707797e8675a87ab768fbc8241d958237fd15e43
Ashish Kumar Tiwari <ashish1004tiwari@gmail.com>
2026-07-07 21:42:31 +0530

    Fix formatting of reporter configuration in Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,11 +33,8 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-<<<<<<< HEAD
-  reporter: [['allure-playwright' , {open : 'never'}],  ['junit' , {outputFile : 'test-results/junit-report.xml'}]],
-=======
   reporter: [['allure-playwright' , {open : 'never'}],   ['junit' , {outputFile : 'test-results/junit-report.xml'}] ],
->>>>>>> 53f3af690a594124be83a806dac1aef00a413bc4
+ 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
